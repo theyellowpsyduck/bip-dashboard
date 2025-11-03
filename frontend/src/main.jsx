5ea66445a0d6233140c085561029bf9724fdc809
theyellowpsyduck <theyellowpsyduck@proton.me>
2025-11-04 04:55:52 +0530

    posthog env variables

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -12,7 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
         defaults: '2025-05-24',
         capture_exceptions: true, // This enables capturing exceptions using Error Tracking, set to false if you don't want this
-        // debug: import.meta.env.MODE === 'development',
+        debug: import.meta.env.MODE === 'development',
       }}
     >
       <App />
